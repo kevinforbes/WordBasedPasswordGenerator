364b5085f6b9fa9642b5c588e0497c03c78e1665
kevinforbes <kevinforbes@gmail.com>
2014-01-03 10:06:47 -0500

    first commit

---
+++ WordBasedPasswordGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace WordBasedPasswordGenerator
+{
+    public static class WordBasedPasswordGenerator
+    {
+        private static readonly Random _random = new Random();
+
+        public static string GeneratePassword()
+        {
+            //get 2 words that are 5 or less characters long
+
+            int count = WordList.Words.Count(x => x.Length <= 5 && x.Length >= 3);
+            string word1 = WordList.Words.Where(x => x.Length <= 5 && x.Length >= 3).ElementAt(_random.Next(count));
+            string word2 = WordList.Words.Where(x => x.Length <= 5 && x.Length >= 3).ElementAt(_random.Next(count));
+
+            //add a number in between
+            return word1 + _random.Next(100) + word2;
+        }
+
+    }
+}
